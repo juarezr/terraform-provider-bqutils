d0abd0933e554b10e1c048d57dc06443b77afbc0
Juarez Rudsatz <juarezr@gmail.com>
2026-07-22 01:23:04 -0300

    docs: improve bqutils_view_parser datasource examples

--- examples/data-sources/bqutils_view_parser/data-source.materialized.tf
+++ examples/data-sources/bqutils_view_parser/data-source.materialized.tf
@@ -1,30 +1,15 @@
-# Parse a MATERIALIZED VIEW and create google_bigquery_table.
+# Load the MATERIALIZED VIEW SQL from a file in the same folder as the Terraform code.
 
-data "bqutils_view_parser" "materialized_view" {
-  sql = <<EOF
-    CREATE OR REPLACE MATERIALIZED VIEW `mydataset`.my_materialized_view
-    PARTITION BY DATE(creation_time)
-    CLUSTER BY customer_name, order_id
-    OPTIONS(
-      description="Materialized view created by Terraform",
-      enable_refresh=TRUE,
-      allow_non_incremental_definition=FALSE,
-      refresh_interval_minutes=60,
-      max_staleness=INTERVAL 90 MINUTE,
-      kms_key_name="projects/1234567890/locations/global/keyRings/my-key-ring/cryptoKeys/my-key",
-      labels=[("org_unit", "development")]
-    ) AS
-      SELECT order_id
-        , customer_name
-        , delivery_type
-        , creation_time
-      FROM mydataset.orders AS o;
-  EOF
+data "bqutils_view_parser" "example" {
+  sql = file("${path.module}/mydataset.my_materialized_view.sql")
 }
 
+# Get the BigQuery dataset to create the MATERIALIZED VIEW in.
 data "google_bigquery_dataset" "mydataset" {
   dataset_id = "mydataset"
 }
+
+# Create the MATERIALIZED VIEW in BigQuery using the attributes parsed from the SQL file.
 
 resource "google_bigquery_table" "materialized_view" {
   dataset_id = data.google_bigquery_dataset.mydataset.dataset_id
--- examples/data-sources/bqutils_view_parser/data-source.tf
+++ examples/data-sources/bqutils_view_parser/data-source.tf
@@ -1,5 +1,5 @@
-# Load VIEW SQL from a file.
+# Load the VIEW SQL from a file in the same folder as the Terraform code.
 
 data "bqutils_view_parser" "example" {
-  sql = file("${path.module}/view.sql")
+  sql = file("${path.module}/mydataset.my_simple_view.sql")
 }
