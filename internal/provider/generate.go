b33d6a197899a6a1151b5dd8c8ae9f356c9a4779
Juarez Rudsatz <juarezr@gmail.com>
2026-08-12 16:11:08 -0300

    fix: update go:generate command for bqutils provider documentation generation
    - Modified the go:generate command in generate.go to specify the provider directory, ensuring correct documentation generation for the bqutils provider.

--- internal/provider/generate.go
+++ internal/provider/generate.go
@@ -1,6 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
-
 package provider
 
 // Generate documentation with:
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name bqutils
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir ../.. --provider-name bqutils
